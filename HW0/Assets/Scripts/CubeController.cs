19667a727c9592c043735fba2ef9c8b7e5a2b658
inankazanci <inankazanci@gmail.com>
2026-02-24 21:06:31 +0300

    Stage 2.11: added movement to cube with wasd.

--- HW0/Assets/Scripts/CubeController.cs
+++ HW0/Assets/Scripts/CubeController.cs
@@ -8,7 +8,12 @@ public class CubeController : MonoBehaviour
     private Rigidbody _rb;
     private Renderer _renderer;
     [SerializeField] private float magnitude = 20f;
+    [SerializeField] private float moveSpeed = 10f;
     private bool _jumping = false;
+    private bool w = false;
+    private bool a = false;
+    private bool s = false;
+    private bool d = false;
 
     private void Awake()
     {
@@ -25,6 +30,27 @@ public class CubeController : MonoBehaviour
             _rb.AddForce(Vector3.up * magnitude);
             _jumping = false;
         }
+
+        if (w)
+        {
+            _rb.AddForce(Vector3.forward * moveSpeed);
+            w = false;
+        }
+        if (a)
+        {
+            _rb.AddForce(Vector3.left * moveSpeed);
+            a = false;
+        }
+        if (s)
+        {
+            _rb.AddForce(Vector3.back * moveSpeed);
+            s = false;
+        }
+        if (d)
+        {
+            _rb.AddForce(Vector3.right * moveSpeed);
+            d = false;
+        }
     }
 
     void Update()
@@ -32,6 +58,22 @@ public class CubeController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             _jumping = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            w = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            a = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            s = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            d = true;
         }
     }
 
