24fe9e41479536fda659de61c54bad49f3e02fd6
I-Kazanci <inankazanci@gmail.com>
2026-03-13 10:50:09 +0300

    Player movements done better

---
+++ HW1/Assets/CameraController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivity = 200f;
+    public Transform playerBody;
+
+    float xRotation = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = true;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        // vertical camera rotation
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -80f, 80f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        // horizontal player rotation
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
--- HW1/Assets/Scripts/PlayerMovement.cs
+++ HW1/Assets/Scripts/PlayerMovement.cs
@@ -3,20 +3,34 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
+
     private CharacterController _controller;
+    private Animator _animator;
 
     void Start()
     {
         _controller = GetComponent<CharacterController>();
+        _animator = GetComponentInChildren<Animator>();
     }
 
     void Update()
     {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
+        
+        Vector3 move = transform.right * h + transform.forward * v;
 
-        Vector3 move = new Vector3(h, 0, v);
+        bool isMoving = move.sqrMagnitude > 0.01f;
+        _animator.SetBool("isMoving", isMoving);
 
-        _controller.Move(move * speed * Time.deltaTime);
+        if (isMoving)
+        {
+            move.Normalize();
+
+            _animator.SetFloat("inputX", h);
+            _animator.SetFloat("inputY", v);
+
+            _controller.Move(move * speed * Time.deltaTime);
+        }
     }
 }
